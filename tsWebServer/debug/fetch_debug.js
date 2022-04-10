9af1e10f6cad5131f33111708cb4dbb126f4f54e
Micha Levy <themicha546@gmail.com>
2022-04-10 17:03:10 +0300

    delete jsServer and move debug folder

--- tsWebServer/debug/fetch_debug.js
+++ tsWebServer/debug/fetch_debug.js
@@ -1,13 +1,16 @@
-const fs = require("fs");
-const { clearInterval } = require("timers");
-const axios = require("axios").default;
+import fs from "fs";
+import { clearInterval } from "timers";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import axios from "axios";
 
+const absdir = dirname(fileURLToPath(import.meta.url)); // hack because __dirname isn't defined in modules
 const range = (n) => [...Array(n).keys()];
 
 const REQ_NUM = process.argv.length <= 2 ? 20 : parseInt(process.argv[2]);
 const USER_NUM = process.argv.length <= 3 ? 1 : parseInt(process.argv[3]);
 const PICTURE_PATH =
-  process.argv.length <= 4 ? __dirname + "/img_debug.txt" : process.argv[4];
+  process.argv.length <= 4 ? absdir + "/img_debug.txt" : process.argv[4];
 
 console.log(`REQ_NUM = ${REQ_NUM}, USER_NUM = ${USER_NUM}`);
 
