a4ec57d4ceb9164db6770785ebe6d7a8a649b9a9
or lantsman <or474894@gmail.com>
2022-05-03 16:37:03 +0300

    adding text to speech

--- cameraApp/src/components/Pages/LearningSignLanguagePage.js
+++ cameraApp/src/components/Pages/LearningSignLanguagePage.js
@@ -1,15 +1,21 @@
 import {View, Text, TextInput, ScrollView, StyleSheet} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import FontName from '../General/FontName';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {PressableOpacity} from 'react-native-pressable-opacity';
 import SignifyHeader from '../General/SignifyHeader';
+import Tts from '../../Utils/text-to-speech';
 const LearningSignLanguagePage = () => {
   const [text, setText] = useState('');
   const [signText, setSignText] = useState('');
 
   const onTranslateButtonPressed = () => {
     setSignText(text);
+  };
+
+  const onTextToSpeechPressed = () => {
+    Tts.say(text);
   };
 
   return (
@@ -33,7 +39,15 @@ const LearningSignLanguagePage = () => {
             size={40}
           />
         </PressableOpacity>
+
+        <PressableOpacity
+          style={{marginLeft: 5}}
+          disabledOpacity={0.4}
+          onPress={onTextToSpeechPressed}>
+          <MaterialCommunityIcons name="volume-high" color="black" size={50} />
+        </PressableOpacity>
       </View>
+
       <ScrollView style={{marginBottom: 120}}>
         <Text style={styles.signText}>{signText} </Text>
       </ScrollView>
