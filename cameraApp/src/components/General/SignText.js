d3413fed588fabf2d22e560daa2f5b6dd5c022e7
or lantsman <or474894@gmail.com>
2022-07-14 17:39:37 +0300

    improving learning page and translate page

--- cameraApp/src/components/General/SignText.js
+++ cameraApp/src/components/General/SignText.js
@@ -1,18 +1,65 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useImperativeHandle, useState} from 'react';
 import FontName from '../General/FontName';
-const SignText = ({
-  text,
-  fontSize = styles.signText.fontSize,
-  color = styles.signText.color,
-  detectedLettersNumber = 0,
-}) => {
+const SignText = (
+  {
+    text,
+    fontSize = styles.signText.fontSize,
+    color = styles.signText.color,
+    detectedColor = 'green',
+    onLetterDetected,
+    onTextDetected,
+  },
+  ref,
+) => {
+  const [detectedLettersCount, setDetectedLettersCount] = useState(0);
+  useEffect(() => {
+    setDetectedLettersCount(0);
+  }, [text]);
+
+  useImperativeHandle(ref, () => {
+    return {
+      detect_letter: detect_letter,
+      get_detected_letters_count: get_detected_letters_count,
+    };
+  });
+
+  const detect_letter = letter => {
+    const text_no_spaces = [...text].filter(x => x != ' ');
+    if (detectedLettersCount >= text.length) {
+      return false;
+    }
+
+    const detected_current_letter =
+      text_no_spaces[detectedLettersCount] == letter;
+    if (detected_current_letter) {
+      setDetectedLettersCount(detectedLettersCount + 1);
+      if (onLetterDetected) {
+        onLetterDetected(
+          letter,
+          detectedLettersCount + 1,
+          text.length - (detectedLettersCount + 1),
+        );
+
+        if (onTextDetected) {
+          onTextDetected();
+        }
+      }
+    }
+
+    return detected_current_letter;
+  };
+
+  const get_detected_letters_count = () => {
+    return detectedLettersCount;
+  };
+
   const getLetterColor = (letter, index) => {
-    return index < detectedLettersNumber ? 'green' : color;
+    return index < detectedLettersCount ? detectedColor : color;
   };
   let letter_index = 0; // only the indexes of chars that are not ' '
   return (
-    <Text style={styles.signText}>
+    <Text style={{...styles.signText, fontSize}}>
       {text.split('').map((letter, index) => {
         letter_color = getLetterColor(letter, letter_index);
         letter_index += letter != ' ';
@@ -34,4 +81,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default SignText;
+export default React.forwardRef(SignText);
