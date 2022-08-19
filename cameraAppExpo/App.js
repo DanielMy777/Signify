04137fab4a4ca165a78be09bc6b0a6ce2fcf4edb
Daniel Malky <DanielMy@mta.ac.il>
2022-08-19 15:36:15 +0300

    Saving space

--- cameraAppExpo/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Text} from 'react-native'
-import SignifyApp from './src/components/Main/SignifyApp';
-
-const App = () => {
-  return <SignifyApp/>
-};
-export default App;
--- cameraAppExpo/src/components/Pages/PageName.js
+++
@@ -1,8 +0,0 @@
-class PageName {
-  static HomePage = 'HomePage';
-  static LearningPage = 'LearningPage';
-  static SettingsPage = 'SettingsPage';
-  static CameraPage = 'CameraPage';
-}
-
-export default PageName;
