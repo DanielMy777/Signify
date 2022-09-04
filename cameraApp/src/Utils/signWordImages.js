4e35abcf6a4628a13e3fb42893d4259503f1539c
Or Lantsman <or474894@gmail.com>
2022-09-04 20:06:55 +0300

    Update signWordImages.js

--- cameraApp/src/Utils/signWordImages.js
+++ cameraApp/src/Utils/signWordImages.js
@@ -7,7 +7,7 @@ signWordsImages = [
   {word: 'hamburger', url: 'asset:/hamburger.png'},
   {word: 'i', url: 'asset:/i.png'},
   {word: 'no', url: 'asset:/no.png'},
-  {word: 'to', url: 'asset:/iloveyou.png'},
+  {word: 'to', url: 'asset:/to.png'},
   {word: 'want', url: 'asset:/want.png'},
   {word: 'you', url: 'asset:/you.png'},
 ];
