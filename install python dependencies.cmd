101ba05908034d86f7ec3033c04f6ebac54893f2
Daniel Malky <79100490+DanielMy777@users.noreply.github.com>
2022-09-10 12:40:38 +0300

    Update install python dependencies.cmd

--- install python dependencies.cmd
+++ install python dependencies.cmd
@@ -1,4 +1,4 @@
-pip install -r prequirments.txt
+pip install -r requirements.txt
 pip uninstall torch
 pip uninstall torchvision
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
